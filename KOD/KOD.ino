874054c43023eb3e893e1933d868d7a2d9bfb2b4
EbruAkileKOC <48384868+EbruAkileKOC@users.noreply.github.com>
2019-05-19 05:09:39 +0300

    Update KOD.ino

--- KOD/KOD.ino
+++ KOD/KOD.ino
@@ -1,19 +1,44 @@
+#define Buton 5
 int sensor=8;
+int in1=7;
+int in2=9;
+int enA=6;
+
 void setup() {
+  pinMode(in1,OUTPUT);
+  pinMode(in2,OUTPUT);
   pinMode (sensor, INPUT);
+  pinMode(Buton,INPUT);
   Serial.begin(9600);
+
+ digitalWrite(in1,LOW);
+ digitalWrite(in2,LOW);
+ digitalWrite(enA,HIGH);
 }
 
 void loop() {
  bool okunan = digitalRead(sensor);
  if(okunan==false){
   Serial.println("yagmur var");
+  digitalWrite(in1,HIGH);
+  analogWrite(enA,50);
+  delay(2000);
+  analogWrite(enA,0);
+  digitalWrite(in1,LOW);
   
   }
-  else{
+  if else(digitalRead(Buton)== 1){
     Serial.println("yagmur yok");
+    digitalWrite(in2,HIGH);
+    analogWrite(enA,50);
+    
     }
-  delay(200);
+  if else(digitalRead(Buton)== 0){
+    digitalWrite(in2,LOW);
+    analogWrite(enA,0);
+    
+    }  
+  
   
   
 }
