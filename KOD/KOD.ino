586241a9ae5576d909b8786d6caed75a1393c80d
EbruAkileKOC <48384868+EbruAkileKOC@users.noreply.github.com>
2019-05-19 05:23:26 +0300

    Update KOD.ino

--- KOD/KOD.ino
+++ KOD/KOD.ino
@@ -1,39 +1,41 @@
-#define Buton 5
+#define Buton 5  // pin tanımlamaları
 int sensor=8;
 int in1=7;
 int in2=9;
 int enA=6;
 
 void setup() {
-  pinMode(in1,OUTPUT);
+  pinMode(in1,OUTPUT);  // pinlerin giriş veya çıkış olma durumları tanımlamaları
   pinMode(in2,OUTPUT);
   pinMode (sensor, INPUT);
   pinMode(Buton,INPUT);
-  Serial.begin(9600);
+  Serial.begin(9600);  // seri portumuzu 9600 baud olarak ayarlıyoruz.
 
- digitalWrite(in1,LOW);
+ digitalWrite(in1,LOW);  // sistem ilk çalıştığında motorun durum tanımlaması.
  digitalWrite(in2,LOW);
  digitalWrite(enA,HIGH);
 }
 
 void loop() {
- bool okunan = digitalRead(sensor);
- if(okunan==false){
-  Serial.println("yagmur var");
+
+ if(digitalRead(sensor)==false){  // Rain sensörden gelen bilgiyi karşılaştırma.
+  Serial.println("yagmur var");  // yağmur varsa motor ileri yöde çalışır ve bir motora bağlanmış mil üzerine sarılı olan sıvı geçirmez perde yavaş hızla açılır.
   digitalWrite(in1,HIGH);
   analogWrite(enA,50);
-  delay(2000);
+  delay(2000);                 // 2 dakikada tamamen açılır ve motor durur.
   analogWrite(enA,0);
   digitalWrite(in1,LOW);
   
   }
-  if else(digitalRead(Buton)== 1 && okunan==true){
+  if else(digitalRead(Buton)== 1 ){  // Butona basılı tutulduğu sürece motor geri yönde çalışıyor.
     Serial.println("yagmur yok");
+    digitalWrite(in1,LOW);  // motor ileri yönde çalışırken butona basılırsa diye alınmış bir önlem.
+    analogWrite(in1,0);
     digitalWrite(in2,HIGH);
     analogWrite(enA,50);
     
     }
-  if else(digitalRead(Buton)== 0){
+  if else(digitalRead(Buton)== 0){  // Butona basılmazsa motor duruyor.
     digitalWrite(in2,LOW);
     analogWrite(enA,0);
     
